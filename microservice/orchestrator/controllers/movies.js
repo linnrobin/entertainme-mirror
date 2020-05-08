5af78cbf92080a31c96fe94195194af910465f6d
robin widjaja <robinsalim@yahoo.com>
2020-05-08 18:47:01 +0700

    finish restAPI

--- microservice/orchestrator/controllers/movies.js
+++ microservice/orchestrator/controllers/movies.js
@@ -22,34 +22,24 @@ class Controller {
     }
   }
 
-  //this method is troublesome... because we need to check the findAll
   static async findById(req, res, next) {
     try {
+      const { data } = await axios({
+        url: baseUrl,
+        method: "get",
+      });
       const movies = JSON.parse(await redis.get("movies"));
       if (movies) {
-        const filtered = movies.filter((el) => el._id === req.params.id);
-        if (filtered.length !== 0) {
-          res.status(200).json(filtered);
-        } else {
-          const { data } = await axios({
-            url: baseUrl + req.params.id,
-            method: "get",
-          });
-          res.status(200).json([data]);
-          if (data !== null) {
-            movies.push(data);
-            redis.set("movies", JSON.stringify(movies));
-          }
-        }
+        redis.del("movies");
+        redis.set("movies", JSON.stringify(data));
       } else {
-        const { data } = await axios({
-          url: baseUrl + req.params.id,
-          method: "get",
-        });
-        res.status(200).json([data]);
-        if (data !== null) {
-          redis.set("movies", JSON.stringify([data]));
-        }
+        redis.set("movies", JSON.stringify(data));
+      }
+      const [filtered] = data.filter((el) => el._id === req.params.id);
+      if (filtered !== undefined) {
+        res.status(200).json(filtered);
+      } else {
+        res.status(404).json({ message: "not found" });
       }
     } catch (error) {
       next(error);
@@ -67,16 +57,78 @@ class Controller {
       const movies = JSON.parse(await redis.get("movies"));
       if (movies) {
         movies.push(data);
+        redis.del("movies");
         redis.set("movies", JSON.stringify(movies));
+      } else {
+        const { data: dataGet } = await axios({
+          url: baseUrl,
+          method: "get",
+        });
+        redis.set("movies", JSON.stringify(dataGet));
       }
     } catch (error) {
       next(error);
     }
   }
 
-  static async delete(req, res, next) {}
+  static async delete(req, res, next) {
+    try {
+      const { data } = await axios({
+        url: baseUrl + req.params.id,
+        method: "delete",
+      });
+      res.status(200).json(data);
+      const movies = JSON.parse(await redis.get("movies"));
+      if (movies) {
+        const notFiltered = movies.filter((el) => el._id !== req.params.id);
+        redis.del("movies");
+        redis.set("movies", JSON.stringify(notFiltered));
+      } else {
+        const { data: dataGet } = await axios({
+          url: baseUrl,
+          method: "get",
+        });
+        redis.set("movies", JSON.stringify(dataGet));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
 
-  static async put(req, res, next) {}
+  static async put(req, res, next) {
+    try {
+      const { data } = await axios({
+        url: baseUrl + req.params.id,
+        method: "put",
+        data: req.body,
+      });
+      res.status(200).json(data);
+      const movies = JSON.parse(await redis.get("movies"));
+      if (movies) {
+        const notFiltered = movies.filter((el) => el._id !== req.params.id);
+        const [filtered] = movies.filter((el) => el._id === req.params.id);
+
+        if (filtered !== undefined) {
+          filtered.title = req.body.title || null;
+          filtered.overview = req.body.overview || null;
+          filtered.poster_path = req.body.poster_path || null;
+          filtered.popularity = req.body.popularity || null;
+          filtered.tags = req.body.tags || null;
+          notFiltered.push(filtered);
+          redis.del("movies");
+          redis.set("movies", JSON.stringify(notFiltered));
+        }
+      } else {
+        const { data: dataGet } = await axios({
+          url: baseUrl,
+          method: "get",
+        });
+        redis.set("movies", JSON.stringify(dataGet));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = Controller;
--- microservice/orchestrator/controllers/tv.js
+++ microservice/orchestrator/controllers/tv.js
@@ -22,34 +22,24 @@ class Controller {
     }
   }
 
-  //this method is troublesome... because we need to check the findAll
   static async findById(req, res, next) {
     try {
+      const { data } = await axios({
+        url: baseUrl,
+        method: "get",
+      });
       const tv = JSON.parse(await redis.get("tv"));
       if (tv) {
-        const filtered = tv.filter((el) => el._id === req.params.id);
-        if (filtered.length !== 0) {
-          res.status(200).json(filtered);
-        } else {
-          const { data } = await axios({
-            url: baseUrl + req.params.id,
-            method: "get",
-          });
-          res.status(200).json([data]);
-          if (data !== null) {
-            tv.push(data);
-            redis.set("tv", JSON.stringify(tv));
-          }
-        }
+        redis.del("tv");
+        redis.set("tv", JSON.stringify(data));
       } else {
-        const { data } = await axios({
-          url: baseUrl + req.params.id,
-          method: "get",
-        });
-        res.status(200).json([data]);
-        if (data !== null) {
-          redis.set("tv", JSON.stringify([data]));
-        }
+        redis.set("tv", JSON.stringify(data));
+      }
+      const [filtered] = data.filter((el) => el._id === req.params.id);
+      if (filtered !== undefined) {
+        res.status(200).json(filtered);
+      } else {
+        res.status(404).json({ message: "not found" });
       }
     } catch (error) {
       next(error);
@@ -68,16 +58,78 @@ class Controller {
 
       if (tv) {
         tv.push(data);
+        redis.del("tv");
         redis.set("tv", JSON.stringify(tv));
+      } else {
+        const { data: dataGet } = await axios({
+          url: baseUrl,
+          method: "get",
+        });
+        redis.set("tv", JSON.stringify(dataGet));
       }
     } catch (error) {
       next(error);
     }
   }
 
-  static async delete(req, res, next) {}
+  static async delete(req, res, next) {
+    try {
+      const { data } = await axios({
+        url: baseUrl + req.params.id,
+        method: "delete",
+      });
+      res.status(200).json(data);
+      const tv = JSON.parse(await redis.get("tv"));
+      if (tv) {
+        const notFiltered = tv.filter((el) => el._id !== req.params.id);
+        redis.del("tv");
+        redis.set("tv", JSON.stringify(notFiltered));
+      } else {
+        const { data: dataGet } = await axios({
+          url: baseUrl,
+          method: "get",
+        });
+        redis.set("tv", JSON.stringify(dataGet));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
 
-  static async put(req, res, next) {}
+  static async put(req, res, next) {
+    try {
+      const { data } = await axios({
+        url: baseUrl + req.params.id,
+        method: "put",
+        data: req.body,
+      });
+      res.status(200).json(data);
+      const tv = JSON.parse(await redis.get("tv"));
+      if (tv) {
+        const notFiltered = tv.filter((el) => el._id !== req.params.id);
+        const [filtered] = tv.filter((el) => el._id === req.params.id);
+
+        if (filtered !== undefined) {
+          filtered.title = req.body.title || null;
+          filtered.overview = req.body.overview || null;
+          filtered.poster_path = req.body.poster_path || null;
+          filtered.popularity = req.body.popularity || null;
+          filtered.tags = req.body.tags || null;
+          notFiltered.push(filtered);
+          redis.del("tv");
+          redis.set("tv", JSON.stringify(notFiltered));
+        }
+      } else {
+        const { data: dataGet } = await axios({
+          url: baseUrl,
+          method: "get",
+        });
+        redis.set("tv", JSON.stringify(dataGet));
+      }
+    } catch (error) {
+      next(error);
+    }
+  }
 }
 
 module.exports = Controller;
